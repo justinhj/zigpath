023ab60590d66d09f6231785e32cf5eb6e5dd846
Justin Heyes-Jones <justinhj@gmail.com>
2025-02-05 19:40:10 -0800

    basic queue working

--- src/queue.zig
+++ src/queue.zig
@@ -2,6 +2,10 @@ const std = @import("std");
 const maxInt = std.math.maxInt(usize);
 const ArrayList = std.ArrayList;
 
+const QueueError = error{
+    OutOfMemory,
+};
+
 pub fn Queue(comptime Child: type) type {
     return struct {
         const This = @This();
@@ -10,7 +14,7 @@ pub fn Queue(comptime Child: type) type {
         end: usize,
         items: []Child,
 
-        pub fn init(gpa: std.mem.Allocator, capacity: usize) !This {
+        pub fn init(gpa: std.mem.Allocator, capacity: usize) QueueError!This {
             const items = try gpa.alloc(Child, capacity);
             return This{
                 .gpa = gpa,
@@ -25,13 +29,34 @@ pub fn Queue(comptime Child: type) type {
             this.gpa.free(this.items);
         }
         pub fn enqueue(this: *This, value: Child) !void {
-            _ = this;
-            _ = value;
+            if (this.start == maxInt) {
+                this.start = 0;
+                this.end = 0;
+                this.items[this.start] = value;
+            } else {
+                const newEnd = (this.end + 1) % this.items.len;
+                if (newEnd == this.start) {
+                    return QueueError.OutOfMemory;
+                } else {
+                    this.end = newEnd;
+                    this.items[this.end] = value;
+                }
+            }
             return {};
         }
         pub fn dequeue(this: *This) ?Child {
-            _ = this;
-            return null;
+            if (this.start == maxInt) {
+                return null;
+            } else {
+                const value = this.items[this.start];
+                if (this.start == this.end) {
+                    this.start = maxInt;
+                    this.end = maxInt;
+                } else {
+                    this.start = (this.start + 1) % this.items.len;
+                }
+                return value;
+            }
         }
     };
 }
@@ -55,4 +80,6 @@ test "queue" {
     try int_queue.enqueue(5);
     try testing.expectEqual(5, int_queue.dequeue());
     try testing.expectEqual(null, int_queue.dequeue());
+
+    int_queue.deinit();
 }
